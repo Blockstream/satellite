7b37dd9d080429938a598c21ee12a4b74e59e0f6
Abylay Ospan <aospan@netup.ru>
2018-01-14 06:35:06 -0500

    tune docker entrypoint script
     * prevent stdin closing when docker-compose used
     * do not start vnc if VNCPASSWORD not set
     * restart reception script if exited (watchdog)
    
    Signed-off-by: Abylay Ospan <aospan@netup.ru>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-set -e
+set -x
 
 #check environment variables first
 [ -z $FREQ ] && { echo "specify -e FREQ=1429000000"; exit 1;}
 [ -z $GAIN ] && { echo "-e GAIN=40 used by default"; export GAIN=40;}
 
 #start vnc server if start script exist
-if [ -f /vncserver.sh ]; then
+if [ -f /vncserver.sh ] && [ ! -z "$VNCPASSWORD" ]; then
   /vncserver.sh &
 fi
 
 #start actual data reception from satellite
+ulimit -c unlimited
 cd /opt/satellite/grc
-if [ -z "$GUI" ]
-then
-  ./rx.py  --freq $FREQ --gain $GAIN
-else
-  DISPLAY=:1 ./rx_gui.py --freq $FREQ --gain $GAIN
-fi
+echo "start rx"
+while [ 1 ];
+do
+  if [ -z "$GUI" ]
+  then
+    # prevent stdin closing when docker-compose used
+    sleep infinity | ./rx.py --freq $FREQ --gain $GAIN
+  else
+    DISPLAY=:1 ./rx_gui.py --freq $FREQ --gain $GAIN
+  fi
+  echo "rx done"
+  sleep 1
+done
