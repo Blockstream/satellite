666c79d4f81203e20dd995aa39fd199915eed48f
Blockstream Satellite <satellite@blockstream.com>
2020-04-15 13:08:03 -0300

    Default to configured net dev for standalone demod
    When running the configuration helper, the user specifies which network
    interface is connected to the standalone demod. By default, use this interface
    when launching the standalone config step. If the user explicitly provides an
    interface via option -i/--interface, give preference to the provided interface.

--- blocksatcli/standalone.py
+++ blocksatcli/standalone.py
@@ -1,6 +1,6 @@
 """Standalone Demodulator"""
 from argparse import ArgumentDefaultsHelpFormatter
-from . import rp, firewall, defs
+from . import rp, firewall, defs, config
 
 
 def subparser(subparsers):
@@ -10,15 +10,28 @@ def subparser(subparsers):
                               help='Configure host to receive from \
                               standalone DVB-S2 demodulator',
                               formatter_class=ArgumentDefaultsHelpFormatter)
-    p.add_argument('-i', '--interface', required = True,
-                   help='Network interface')
+    p.add_argument('-i', '--interface',
+                    default=None,
+                    help='Network interface connected to the standalone \
+                    demodulator')
     p.set_defaults(func=cfg_standalone)
     return p
 
 def cfg_standalone(args):
     """Configurations for standalone DVB demodulator
     """
-    rp.set_rp_filters([args.interface])
-    firewall.configure([args.interface], defs.src_ports, igmp=True)
+    # User info
+    user_info = config.read_cfg_file(args.cfg_file, args.cfg_dir)
+
+    if 'netdev' not in user_info['setup']:
+        assert(args.interface is not None), \
+            ("Please specify the network interface through option "
+             "\"-i/--interface\"")
+
+    interface = args.interface if (args.interface is not None) else \
+                user_info['setup']['netdev']
+
+    rp.set_rp_filters([interface])
+    firewall.configure([interface], defs.src_ports, igmp=True)
 
 
