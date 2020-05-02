3330eb7b3f5ea7c81dcc667a85b06527d9c8a60d
Blockstream Satellite <satellite@blockstream.com>
2020-05-01 21:04:13 -0300

    Prevent python2 installation via setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
-import re
+import re, sys
 from setuptools import setup
 
+
+if sys.version_info[0] < 3:
+    raise SystemExit("Error: blocksat-cli requires Python 3")
+    sys.exit(1)
 
 version = re.search(
     '^__version__\s*=\s*"(.*)"',
@@ -8,6 +12,12 @@ version = re.search(
     re.M
 ).group(1)
 
+long_description = """# Blockstream Satellite CLI
+
+A command-line interface for configuring, running and monitoring a Blockstream
+Satellite receiver setup.
+
+"""
 
 setup(
     name = "blocksat-cli",
@@ -17,11 +27,15 @@ setup(
     },
     version = version,
     description = "Blockstream Satellite CLI",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author = "Blockstream Corp",
     author_email = "satellite@blockstream.com",
     url = "https://github.com/Blockstream/satellite",
     classifiers=[
-        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3',
+        "Programming Language :: Python :: 3 :: Only",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
     ],
+    python_requires='>=3'
 )
